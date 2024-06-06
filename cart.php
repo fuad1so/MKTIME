f1b643d4c066044bf207b0edb8775fb459f7127a
Fuad alsohli <fuadsohly@gmail.com>
2024-06-07 00:22:09 +0100

    add cart

---
+++ cart.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+require("Database_conection.php");
+
+// Initialize the cart if it doesn't exist
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = [];
+}
+
+// Add item to the cart
+if (isset($_GET['id'])) {
+    $itemId = intval($_GET['id']);
+
+    // Fetch the item details from the database
+    $sqlCheck = "SELECT item_id, item_name, item_desc, item_img, item_price FROM items WHERE item_id = ?";
+    $stmt = mysqli_prepare($link, $sqlCheck);
+    mysqli_stmt_bind_param($stmt, "i", $itemId);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $item_id, $item_name, $description, $image, $price);
+    if (mysqli_stmt_fetch($stmt)) {
+        // Add the item to the cart session
+        $_SESSION['cart'][] = [
+            'item_id' => $item_id,
+            'item_name' => $item_name,
+            'item_desc' => $description,
+            'item_img' => $image,
+            'item_price' => $price
+        ];
+    }
+    mysqli_stmt_close($stmt);
+}
+
+// Remove item from cart
+if (isset($_GET['remove'])) {
+    $removeId = intval($_GET['remove']);
+    unset($_SESSION['cart'][$removeId]);
+    // Reindex array to maintain proper indices
+    $_SESSION['cart'] = array_values($_SESSION['cart']);
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Shopping Cart</title>
+    <link rel="stylesheet" type="text/css" href="stylesCart.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <h1>Your Shopping Cart</h1>
+        <?php if (empty($_SESSION['cart'])) : ?>
+            <p>Your cart is empty.</p>
+        <?php else : ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Item Image</th>
+                        <th>Item Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($_SESSION['cart'] as $index => $item) : ?>
+                        <tr>
+                            <td><img src="img/<?php echo $item['item_img']; ?>" alt="<?php echo $item['item_name']; ?>" width="100"></td>
+                            <td><?php echo $item['item_name']; ?></td>
+                            <td><?php echo $item['item_desc']; ?></td>
+                            <td>£<?php echo $item['item_price']; ?></td>
+                            <td><a href="cart.php?remove=<?php echo $index; ?>">Remove</a></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php endif; ?>
+    </div>
+
+</body>
+
+</html>
--- fashion_section.php
+++ fashion_section.php
@@ -38,8 +38,8 @@ mysqli_stmt_close($stmt);
                                  <h4 class='shirt_text'>{$items[$i]['item_name']}</h4>
                                  <p class='price_text'>Start Price <span style='color:#262626;'>£{$items[$i]['item_price']}</span></p>
                                  <div class='electronic_img'>" . "<img src='img/{$items[$i]['item_img']}'>" . "</div>
-                                    <div class='btn_main'>
-                                         <div class='buy_bt'><a href='#'>Add to Cart</a></div>
+                                    <div class='btn_main'>   
+                                         <div class='buy_bt'><a href= 'cart.php?id=$i'>Add to Cart</a></div>
                                          <div class='seemore_bt'><a href='#'>See More</a></div>
                                     </div>
                                 </div>
